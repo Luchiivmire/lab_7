83d5085b3353dc6aa757db49d21427a08b6285e1
Luchiivmire <nik964ita85@gmail.com>
2023-06-05 21:30:31 +0300

    Я сделал коммит снова

--- Weather.cpp
+++ Weather.cpp
@@ -1,20 +1,24 @@
-﻿// Weather.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+#include "Weather.h"
+Weather::Weather(string city, double lon, double lat, double temperature, string weather, double windSpeed, int clouds) {
 
-#include <iostream>
+    this->city = city;
+    this->lon = lon;
+    this->lat = lat;
+    this->temperature = temperature;
+    this->weather = weather;
+    this->windSpeed = windSpeed;
+    this->clouds = clouds;
 
-int main()
-{
-    std::cout << "Hello World!\n";
 }
 
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+void Weather::getData() {
 
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+    cout << city << ": lon(" << lon << "), lat(" << lat << "), t(" << temperature << "), weather(" << weather << "), wind speed(" << windSpeed << "), clouds(" << clouds << ")\n";
+
+}
+
+std::ostream& operator<<(std::ostream& out, Weather weatherrr)
+{
+    out << weatherrr.city << ": \nlon(" << weatherrr.lon << "); \nlat(" << weatherrr.lat << "); \nt(" << weatherrr.temperature << "); \nweather(" << weatherrr.weather << "); \nwind speed(" << weatherrr.windSpeed << "); \nclouds(" << weatherrr.clouds << ")\n";
+    return out;
+}
